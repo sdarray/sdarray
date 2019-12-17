f750650cba238db2f8d9694f48b7a09610e3d76f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-12-17 14:40:50 +0900

    #10 Release v0.1.2

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,7 @@ import sdarray as sd
 
 def test_version():
     """Make sure the version is valid."""
-    assert sd.__version__ == "0.1.1"
+    assert sd.__version__ == "0.1.2"
 
 
 def test_author():
