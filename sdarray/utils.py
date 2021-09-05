34d896d32ab256fe24a4576d0068fd18cc8c562b
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-09-05 07:07:49 +0000

    #23 Add unwrap to utils module

--- sdarray/utils.py
+++ sdarray/utils.py
@@ -2,8 +2,13 @@
 
 
 # standard library
+import re
 from dataclasses import field
-from typing import TypeVar
+from typing import Optional, TypeVar
+
+
+# constants
+INDENT = re.compile(r"(\s{4})+")
 
 
 # type hints
@@ -14,3 +19,11 @@ T = TypeVar("T")
 def readonly(default: T) -> T:
     """Create a read-only field for dataclasses."""
     return field(default=default, init=False)
+
+
+def unwrap(doc: Optional[str]) -> str:
+    """Unwrap a multi-line docstring."""
+    if doc is None:
+        return ""
+
+    return " ".join(INDENT.sub("", doc).split())
