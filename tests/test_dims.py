af160df379d444a9994c62c312770a545cebbdb4
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-11-10 09:18:04 +0000

    #28 Update test for dims module

--- tests/test_dims.py
+++ tests/test_dims.py
@@ -6,18 +6,18 @@ from xarray_dataclasses import asdataarray
 
 # test functions
 def test_time() -> None:
-    data = np.array(["2000-01-01"], "M8[ns]")
+    data = np.array(["2000-01-01"], np.datetime64)
     time = asdataarray(sd.dims.Time(data))
 
     assert time.data == data
     assert time.dims == ("time",)
-    assert time.dtype == data.dtype
+    assert time.dtype.type == data.dtype.type
 
 
 def test_chan() -> None:
-    data = np.array([0], "i8")
+    data = np.array([0], np.int64)
     chan = asdataarray(sd.dims.Chan(data))
 
     assert chan.data == data
     assert chan.dims == ("chan",)
-    assert chan.dtype == data.dtype
+    assert chan.dtype.type == data.dtype.type
