efd92778bce9607c580679f2179df74f46b0d4ff
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-11-17 06:37:15 +0000

    #30 Add KeywordOnly mixin

---
+++ sdarray/array/mixins.py
@@ -0,0 +1,17 @@
+# standard library
+from typing import Any, Type, TypeVar
+
+
+# type hints
+T = TypeVar("T")
+
+
+# mixin classes
+class KeywordOnly:
+    """Limit the second and subsequent ``__init__`` args to be keyword-only."""
+
+    def __new__(cls: Type[T], *args: Any, **kwargs: Any) -> T:
+        if len(args) > 1:
+            raise TypeError("The second and subsequent args are keyword-only.")
+
+        return super().__new__(cls)
